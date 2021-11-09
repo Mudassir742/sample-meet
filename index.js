e98d457c510f3416ca6286331f25bfdf737ad2f1
Mudassir742 <mudassirwaheed242@gmail.com>
2021-11-10 01:18:03 +0500

    ports

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ app.set("view engine","ejs")
 
 app.get('/', (req, res) => res.render('home'))
 
-const PORT = 5000 || process.env.PORT
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server is Running`))
